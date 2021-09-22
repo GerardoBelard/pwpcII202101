decd576507bb6543b8e2bc470862bd5b2ea3b331
GerardoBelard <belard86@gmail.com>
2021-09-22 14:19:04 -0500

    proyecto en estado inicial funcionando

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', author:'Gerardo', appName: 'Webapp' });
+});
+
+/* Agregando nueva ruta */
+router.get('/greeting', function(req , res, next){
+  res.status(200).json({message:'Que pasa chaval'})
+})
+
+/* Agregando nueva ruta */
+router.get('/Reto', function(req , res, next){
+  res.send('Reto completado')
+})
+
+
+module.exports = router;
