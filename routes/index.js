e5a47d08fbfb3921b078a562f0077cb30ba50361
GerardoBelard <belard86@gmail.com>
2021-09-22 20:13:51 -0500

    Agregando Nombre de compañia

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author:'Gerardo', appName: 'Webapp' });
+  res.render('index', { title: 'Express', author:'Gerardo', appName: 'Webapp', Company: 'Awsome Software'});
 });
 
 /* Agregando nueva ruta */
