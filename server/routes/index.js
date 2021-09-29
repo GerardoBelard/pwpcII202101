8190113f6a5ac9bbfa0c915949809daacf257f97
GerardoBelard <belard86@gmail.com>
2021-09-28 23:44:34 -0500

    babel con rinraf funcionando video 17

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author:'Gerardo', appName: 'Webapp' });
+  res.render('index', { title: 'Express', author:'Elon Musk', appName: 'Webapp' });
 });
 
 /* Agregando nueva ruta */
